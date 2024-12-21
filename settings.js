3e2b6d52fb285438ceeb47d026361f2371f78278
Neggy5 <132104247+Neggy5@users.noreply.github.com>
2024-12-21 04:51:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "234xxxxxxxxx"
+global.pairingNumber = "2347030429025"
 
-global.botNumber = ["234xxxxxxxxx"]
+global.botNumber = ["2347030429025"]
 
 global.anticall = false
 
